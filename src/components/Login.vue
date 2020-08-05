3bbba43106258f463c57adef481c8b1e5f371c27
stevemeister5 <stephenreubenm@gmail.com>
2020-08-05 10:00:11 +0300

    Store dispatch

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -11,11 +11,39 @@
 </template>
 
 <script>
+
+import { mapActions } from 'vuex'
+
 export default {
+
     methods: {
+        
+
         openGoogleSignin () {
-            // TODO
-        }
-    }
+            const url = 'http://localhost:3000/auth/google'
+            const name = 'google_login'
+            const specs = 'width=500, height=500'
+            window.open(url, name, specs)
+        },
+
+        handleMessage ({data, origin}) {
+            if (origin !== 'http://localhost:3000') {
+                return
+            }
+
+            if( data === 'success') {
+                this.$store.dispatch.login()
+            }
+        },
+    },
+
+    mounted () {
+        window.addEventListener('message', this.handleMessage)
+    },
+
+    beforeDestroy () {
+        window.removeEventListener('message', this.handleMessage)
+    },
+
 }
 </script>
