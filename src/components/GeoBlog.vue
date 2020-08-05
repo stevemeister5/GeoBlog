c80818fcdda8061d1d8ef784e28479da3713e511
stevemeister5 <stephenreubenm@gmail.com>
2020-08-05 10:57:39 +0300

    MapVIew

--- src/components/GeoBlog.vue
+++ src/components/GeoBlog.vue
@@ -2,14 +2,20 @@
     <div class="geo-blog">
         <AppMenu />
         <!-- Map & content here -->
+        <div class="panes">
+            <BlogMap />
+        </div>
     </div>
 </template>
 
 <script>
 import AppMenu from './AppMenu'
+import BlogMap from './BlogMap'
+
 export default {
     components: {
         AppMenu,
+        BlogMap,
     },
 }
 </script>
