7aad4e12a1824fdb452d5f644c595cf4777cb42c
stevemeister5 <stephenreubenm@gmail.com>
2020-08-05 03:04:57 +0300

    Initial Setup

---
+++ README.md
@@ -0,0 +1,18 @@
+# geoblog
+
+> A Geobloggin app Using Google Maps and Google Oauth
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import VueFetch, { $fetch } from './plugins/fetch'
+import App from './components/App.cue'
+import router from './router'
+import * as filters from './filters'
+
+// Filters 
+for (const key in filters) {
+    Vue.filter(key, filters[key])
+}
+
+Vue.use(VueFetch, {
+    baseUrl: 'http://localhost:3000',
+})
+
+function main () {
+    new Vue({
+        ...App,
+        el: '#app',
+        router
+    })
+}
+
+main()
