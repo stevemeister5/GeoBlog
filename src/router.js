cbd965369a74bea8179f768e23882a89a0c09f0d
stevemeister5 <stephenreubenm@gmail.com>
2020-08-05 10:23:09 +0300

    Navigation Guards

--- src/router.js
+++ src/router.js
@@ -5,6 +5,8 @@ import Login from './components/Login.vue'
 import GeoBlog from './components/GeoBlog.vue'
 import NotFound from './components/NotFound.vue'
 
+import store from './store'
+
 
 Vue.use(VueRouter)
 
@@ -28,6 +30,23 @@ const router = new VueRouter({
     },
 })
 
-// TODO Navigation guards
+router.beforeEach( (to, from, next) => {
+    console.log('to', to.name)
+    const user = store.getters.user
+    if (to.matched.some(r => r.meta.private) && !user) {
+        next({
+            name: 'login',
+            params: {
+                wantedRoute: to.fullPath,
+            },
+        })
+        return
+    }
+    if (to.matched.some(r => r.meta.guest) && user) {
+        next({ name: 'home' })
+        return
+    }
+    next()
+})
 
 export default router
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
@@ -22,17 +23,33 @@ const store = new Vuex.Store({
         userPicture: () => null,
     },
     actions: {
-        login ({ commit }) {
-            const userData = {
-                profile: {
-                    displayName: 'Mr Cat',
-                },
+        async login ({ commit }) {
+            try {
+                const user = await $fetch('user')
+                commit('user', user)
+
+                if (user) {
+                    // Redirect to the wanted route if any or else to home
+                    router.replace(router.currentRoute.params.wantedRoute || 
+                        { name: 'home' })
+                }
+            } catch (e) {
+                console.warn(e)
             }
-            commit('user', userData)
         },
 
         logout ({ commit }) {
             commit('user', null)
+
+            $fetch('logout')
+
+            // If the route is private
+            // We go to the login screen
+            if (router.currentRoute.matched.some(r => r.meta.private)) {
+                router.replace({ name: 'login', params: {
+                    wantedRoute: router.currentRoute.fullPath,
+                }})
+            }
         },
     },
 })
