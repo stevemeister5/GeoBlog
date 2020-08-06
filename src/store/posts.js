c125bf2571b54bf077e37552cbc2cfcc5e24e67c
stevemeister5 <stephenreubenm@gmail.com>
2020-08-06 23:37:17 +0300

    Post COmments

--- src/store/posts.js
+++ src/store/posts.js
@@ -56,6 +56,9 @@ export default {
         selectedPostDetails (state, value) {
             state.selectedPostDetails = value
         },
+        addComment (state, { post, comment }) {
+            post.comments.push(comment)
+        },
     },
 
     actions: {
@@ -84,6 +87,7 @@ export default {
             },
             root: true,
         },
+
 
         clearDraft ({ commit }) {
             commit('draft', null)
@@ -165,6 +169,24 @@ export default {
                     })
                 }
             }
+        },
+
+        async sendComment({ commit, rootGetters }, { post, comment }) {
+            const user = rootGetters.user
+            commit('addComment', {
+                post,
+                comment: {
+                    ...comment,
+                    date: new Date(),
+                    user_id: user._id,
+                    author: user,
+                },
+            })
+
+            await $fetch(`posts/${pos._id}/comment`, {
+                method: 'POST',
+                body: JSON.stringify(comment),
+            })
         },
     },
 }
