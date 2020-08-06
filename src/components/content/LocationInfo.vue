b8202a0d2f0a06792a96e568b47e0881982f4f62
stevemeister5 <stephenreubenm@gmail.com>
2020-08-06 23:33:32 +0300

    Place Details

--- src/components/content/LocationInfo.vue
+++ src/components/content/LocationInfo.vue
@@ -1,3 +1,37 @@
 <template>
-    
+    <div class="location-info" v-if="currentPost">
+        <!-- Place -->
+        <googlemaps-place-details
+            v-if="currentPost.placeId"
+            :request="{
+                placeId: currentPost.placeId
+                }">
+            <PlaceDetails
+                slot-scope="props"
+                v-if="props.results"
+                :name="props.results[1].PlaceDetails.name"
+                :address="props.results[0].formatted_address"/>
+        </googlemaps-place-details>
+    </div>
+    <div v-else></div>
 </template>
+
+<script>
+import PlaceDetails from './PlaceDetails'
+import { createNamespacedHelpers } from 'vuex'
+
+// posts module
+const {
+    mapGetters: postsGetters,
+} = createNamespacedHelpers('posts')
+
+export default {
+    component: {
+        PlaceDetails,
+    },
+
+    computed: postsGetters([
+        'currentPost',
+    ]),
+}
+</script>
---
+++ src/components/content/PlaceDetails.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    props: {
+        name: String,
+        address: String,
+    },
+
+    render (h) {
+        return <div class="details">
+            <div class="name"><i class="material-icons">place</i>
+                {this.name}</div>
+            <div class="address"> {this.address}</div>
+        </div>
+    },
+}
+</script>
